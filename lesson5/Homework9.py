834075dd92804c23e6ec701ce88654ca4f94c855
DariaUrman <117667016+DariaUrman@users.noreply.github.com>
2023-04-27 16:08:39 +0300

    Homework9.py

--- lesson5/Homework9.py
+++ lesson5/Homework9.py
@@ -46,7 +46,7 @@ def binary_search(r_array, a):
             if a < r_array[mid_val]:
                 last = mid_val - 1
             else:
-                first = mid + 1
+                first = mid_val + 1
         duration_tb = time.time() - start_tb
     return index, duration_tb
 
