64c4603aa5465a143fec36a11bcd35db635548b3
DariaUrman <117667016+DariaUrman@users.noreply.github.com>
2023-05-04 17:53:46 +0300

    Homework10.py

---
+++ lesson5/Homework10.py
@@ -0,0 +1,53 @@
+"""Сгенерируйте массив из целых чисел, содержащий 100 000 элементов, с помощью функции randomint из модуля random.
+
+Сгенерируйте с помощью функции range массив, содержащий словари со следующей структурой:
+
+{
+	“num_1”: randomint(1,1_000_000),
+	“num_2”: randomint(1,1_000_000)
+}
+
+Длина массива должна составлять 100 000 элементов.
+
+Напишите функцию алгоритма сортировки пузырьком и с её помощью отсортируйте первый массив.
+
+Отсортируйте второй массив с помощью встроенного спиского метода .sort() и лямбда-функции сначала по первому ключу, потом по второму
+"""
+import random
+
+array = [random.randint(-50, 50) for i in range(100000)]
+print(array)
+
+new_array = []
+for i in range(100000):
+    new_array.append(
+        {
+            "num_1": random.randint(1, 1000000),
+            "num_2": random.randint(1, 1000000)
+        }
+    )
+print(*new_array, sep="\n")
+
+def bubble_sort(n_array):
+    n = len(n_array)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if n_array[j] > n_array[j + 1]:
+                bubble = n_array[j]
+                n_array[j] = n_array[j + 1]
+                n_array[j + 1] = bubble
+    return n_array
+
+n_array = [random.randint(-50, 50) for i in range(100000)]
+sorted_n_array = bubble_sort(n_array)
+print(sorted_n_array)
+
+new_array = []
+for i in range(100000):
+    new_array.append(
+        {
+            "num_1": random.randint(1, 1000000),
+            "num_2": random.randint(1, 1000000)
+        }
+    )
+print((new_array.sort(key=lambda x: (x[0], x[1]))))
